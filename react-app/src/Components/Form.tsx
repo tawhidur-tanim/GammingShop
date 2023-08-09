99d019ee9d88dfed759c184c4ddb177ea1bf5094
tawhidur-tanim <rahman35-964@diu.edu.bd>
2023-08-09 17:50:08 +0600

    state controlled form

--- react-app/src/Components/Form.tsx
+++ react-app/src/Components/Form.tsx
@@ -1,17 +1,11 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
   const onFormSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("submitted");
-
-    if (nameRef.current) {
-      //   nameRef.current.value = "New Name";
-      console.log(nameRef.current.value);
-    }
   };
 
-  const nameRef = useRef<HTMLInputElement>(null);
+  const [person, setPerson] = useState({ name: "", age: "" });
 
   return (
     <form onSubmit={onFormSubmit}>
@@ -19,14 +13,30 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          value={person.name}
+          onChange={(event) =>
+            setPerson({ ...person, name: event.currentTarget.value })
+          }
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
 
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          id="age"
+          onChange={(event) =>
+            setPerson({ ...person, age: event.currentTarget.value })
+          }
+          value={person.age}
+          type="number"
+          className="form-control"
+        />
       </div>
 
       <button className="btn btn-primary">Submit</button>
