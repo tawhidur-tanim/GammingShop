fd0aa940579b74d8658bda55af371474e7a87370
tawhidur-tanim <rahman35-964@diu.edu.bd>
2023-08-11 13:27:41 +0600

    validation added with errors object and form interface

--- react-app/src/Components/Form.tsx
+++ react-app/src/Components/Form.tsx
@@ -1,8 +1,17 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onFormSubmit = (data: FieldValues) => {
     console.log(data);
@@ -15,11 +24,12 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type == "required" && <p>Name is required</p>}
       </div>
 
       <div className="mb-3">
@@ -27,11 +37,12 @@ const Form = () => {
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { required: true, min: 10 })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age?.type == "required" && <p>Age is required</p>}
       </div>
 
       <button className="btn btn-primary">Submit</button>
