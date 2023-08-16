94fc7e41dadb70b973bb74fb62ba8d939bb61bc5
tawhidur-tanim <rahman35-964@diu.edu.bd>
2023-08-16 13:11:12 +0600

    useEffect with dependecny

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import ExapandableText from "./Components/ExapandableText";
 import Form from "./Components/Form";
+import ProductList from "./Components/ProductList";
 import StateManagement from "./Components/StateManagement";
 
 function App() {
+  const [category, setCategory] = useState("");
   return (
     <>
-      <Form></Form>
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.currentTarget.value)}
+      >
+        <option value="Clothing">Clothing</option>
+        <option value="Kitchen">Kitchen</option>
+        <option value="Electronics">Electronics</option>
+      </select>
+
+      <ProductList category={category}></ProductList>
     </>
   );
 }
---
+++ react-app/src/Components/ProductList.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const ProductList = ({ category }: { category: string }) => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const [ProductList, setProductList] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log("Fetching products using category - " + category);
+    setProductList(["Clothing", "Kitchen"]);
+
+    if (nameRef.current) {
+      nameRef.current.value = category;
+    }
+  }, [category]);
+
+  return (
+    <div>
+      <input type="text" ref={nameRef} id="name" className="form-control" />
+    </div>
+  );
+};
+
+export default ProductList;
