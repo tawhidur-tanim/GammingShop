4ef5ed3d42a386d96cfd287ba44ff107fc557235
tawhidur-tanim <rahman35-964@diu.edu.bd>
2023-08-28 14:00:26 +0600

    dark mode added

--- GameHub/src/App.tsx
+++ GameHub/src/App.tsx
@@ -11,19 +11,15 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav" bg="coral">
+        <GridItem area="nav">
           <NavBar />
         </GridItem>
 
         <Show above="lg">
-          <GridItem area="aside" bg="green">
-            Aside
-          </GridItem>
+          <GridItem area="aside">Aside</GridItem>
         </Show>
 
-        <GridItem area="main" bg="yellow">
-          Main
-        </GridItem>
+        <GridItem area="main">Main</GridItem>
       </Grid>
     </>
   );
