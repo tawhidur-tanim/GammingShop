0f77fd1ca52ee19900025b53ef7a31077b34b9b5
tawhidur-tanim <rahman35-964@diu.edu.bd>
2023-08-28 13:14:17 +0600

    Chakra UI added

--- GameHub/src/App.tsx
+++ GameHub/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button colorScheme="green">Button</Button>
+    </>
+  );
 }
 
 export default App;
