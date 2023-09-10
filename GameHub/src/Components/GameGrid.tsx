e1cea40f13e04b54fc64d6e98c5294d7d8c020e3
tawhidur-tanim <rahman35-964@diu.edu.bd>
2023-09-10 16:43:40 +0600

    Error message added in game grid

--- GameHub/src/Components/GameGrid.tsx
+++ GameHub/src/Components/GameGrid.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import apiClient from "../Services/apiClient";
+import apiClient, { CanceledError } from "../Services/apiClient";
+
+import { Text } from "@chakra-ui/react";
 
 interface Game {
   id: number;
@@ -13,19 +15,24 @@ interface FetchGameResponse {
 
 const GameGrid = () => {
   const [games, setGames] = useState<Game[]>([]);
+  const [error, seterror] = useState("");
 
   useEffect(() => {
     const controller = new AbortController();
     apiClient
       .get<FetchGameResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        seterror(err.message);
+      });
 
     return () => controller.abort();
   }, []);
 
   return (
     <>
+      {error && <Text>{error}</Text>}
       <ul>
         {games.map((game) => (
           <li key={game.id}>{game.name}</li>
--- GameHub/src/Services/apiClient.ts
+++ GameHub/src/Services/apiClient.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
@@ -6,3 +6,5 @@ export default axios.create({
     key: "7c5e9b86844b4459bedf714a4d0dd845",
   },
 });
+
+export { CanceledError };
